71483b28e026ba264da898d80b1e421d89688d21
Soha <36818003+SohaWithAnS@users.noreply.github.com>
2018-07-21 17:07:35 +0530

    Update MainActivity.java
    Remove unnecessary comments

--- app/src/main/java/io/sohaparasnis/courtcounter/MainActivity.java
+++ app/src/main/java/io/sohaparasnis/courtcounter/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /* displayForTeamA(0);*/
     }
 
     public void displayForTeamA(int score) {
